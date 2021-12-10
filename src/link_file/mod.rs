0433280f7830c5f7881f6ace845f63de95c39bd7
Lucky-Skebe <fmann12345@gmail.com>
2021-12-10 18:04:38 +0100

    feat: simple ui to see the current status
    based on the link_file

--- src/link_file/mod.rs
+++ src/link_file/mod.rs
@@ -3,12 +3,273 @@ mod contracts;
 use std::time::Duration;
 
 pub use contracts::*;
+use futures::SinkExt;
 use tokio::io::AsyncReadExt;
 use tracing::{debug, error, info};
 
+struct RunningState {
+    file: tokio::fs::File,
+    loading: bool,
+    starting: bool,
+    index: usize,
+    content: String,
+}
+
+enum LinkFileScannerState {
+    Init(String),
+    Running(RunningState),
+}
+
+pub struct LinkFileScanner(String);
+
+impl LinkFileScanner {
+    pub fn new(path: String) -> Self {
+        Self(path)
+    }
+}
+
+impl<H, I> iced_native::subscription::Recipe<H, I> for LinkFileScanner
+where
+    H: std::hash::Hasher,
+{
+    type Output = Event;
+
+    fn hash(&self, state: &mut H) {
+        use std::hash::Hash;
+
+        std::any::TypeId::of::<Self>().hash(state);
+        self.0.hash(state);
+    }
+
+    fn stream(
+        self: Box<Self>,
+        _input: iced_futures::BoxStream<I>,
+    ) -> iced_futures::BoxStream<Self::Output> {
+        Box::pin(futures::stream::unfold(
+            LinkFileScannerState::Init(self.0.clone()),
+            |state| async move {
+                match state {
+                    LinkFileScannerState::Init(path) => {
+                        let mut file = tokio::fs::File::open(&path).await.ok()?;
+                        let mut content = String::new();
+
+                        let mut starting = true;
+                        let mut loading = false;
+
+                        let mut last_index = 0;
+
+                        file.read_to_string(&mut content).await.ok()?;
+
+                        content = content
+                            .replace("':TRUE", "':true")
+                            .replace("':False", "':false")
+                            .replace("'", "\"");
+
+                        loop {
+                            for (index, char) in content.chars().enumerate() {
+                                if char == '\n' {
+                                    let line = &content[last_index..index];
+                                    last_index = index + 1;
+                                    if line.starts_with("{") {
+                                        if line != "{}" {
+                                            let event = serde_json::from_str::<Event>(line);
+
+                                            match event {
+                                                Ok(Event::Game(GameEvent::LoadingSaveDone)) => {
+                                                    loading = false;
+                                                }
+                                                Ok(ev @ Event::Game(GameEvent::LoadingSave(_))) => {
+                                                    loading = true;
+                                                    return Some((
+                                                        ev,
+                                                        LinkFileScannerState::Running(
+                                                            RunningState {
+                                                                content,
+                                                                file,
+                                                                index,
+                                                                loading,
+                                                                starting,
+                                                            },
+                                                        ),
+                                                    ));
+                                                }
+                                                Ok(ev @ Event::Sla(_))
+                                                | Ok(
+                                                    ev @ Event::DD(DDEvent::EquipmentChanged(_)),
+                                                ) => {
+                                                    info!(?ev, "Handling Event");
+
+                                                    return Some((
+                                                        ev,
+                                                        LinkFileScannerState::Running(
+                                                            RunningState {
+                                                                content,
+                                                                file,
+                                                                index,
+                                                                loading,
+                                                                starting,
+                                                            },
+                                                        ),
+                                                    ));
+                                                }
+                                                Ok(ev) if loading | starting => {
+                                                    debug!(?ev, "Skipping Event");
+                                                }
+                                                Ok(ev) => {
+                                                    info!(?ev, "Handling Event");
+
+                                                    return Some((
+                                                        ev,
+                                                        LinkFileScannerState::Running(
+                                                            RunningState {
+                                                                content,
+                                                                file,
+                                                                index,
+                                                                loading,
+                                                                starting,
+                                                            },
+                                                        ),
+                                                    ));
+                                                }
+                                                Err(e) => {
+                                                    error!(?e, ?line, "Could not Parse Event")
+                                                }
+                                            }
+                                        }
+                                    } else {
+                                        info!("{}", line);
+                                    }
+                                }
+                            }
+                            starting = false;
+
+                            tokio::time::sleep(Duration::from_millis(100)).await;
+
+                            content.clear();
+                            file.read_to_string(&mut content).await.ok()?;
+
+                            content = content
+                                .replace("':TRUE", "':true")
+                                .replace("':False", "':false")
+                                .replace("'", "\"");
+
+                            last_index = 0;
+                        }
+                    }
+                    LinkFileScannerState::Running(RunningState {
+                        content,
+                        starting,
+                        loading,
+                        index,
+                        file,
+                    }) => {
+                        let mut last_index = index + 1;
+                        let mut loading = loading;
+                        let mut starting = starting;
+                        let mut content = content;
+                        let mut file = file;
+
+                        loop {
+                            for (index, char) in content.chars().enumerate().skip(last_index) {
+                                if char == '\n' {
+                                    let line = &content[last_index..index];
+                                    last_index = index + 1;
+                                    if line.starts_with("{") {
+                                        if line != "{}" {
+                                            let event = serde_json::from_str::<Event>(line);
+
+                                            match event {
+                                                Ok(Event::Game(GameEvent::LoadingSaveDone)) => {
+                                                    loading = false;
+                                                }
+                                                Ok(ev @ Event::Game(GameEvent::LoadingSave(_))) => {
+                                                    loading = true;
+                                                    return Some((
+                                                        ev,
+                                                        LinkFileScannerState::Running(
+                                                            RunningState {
+                                                                content,
+                                                                file,
+                                                                index,
+                                                                loading,
+                                                                starting,
+                                                            },
+                                                        ),
+                                                    ));
+                                                }
+                                                Ok(ev @ Event::Sla(_))
+                                                | Ok(
+                                                    ev @ Event::DD(DDEvent::EquipmentChanged(_)),
+                                                ) => {
+                                                    info!(?ev, "Handling Event");
+
+                                                    return Some((
+                                                        ev,
+                                                        LinkFileScannerState::Running(
+                                                            RunningState {
+                                                                content,
+                                                                file,
+                                                                index,
+                                                                loading,
+                                                                starting,
+                                                            },
+                                                        ),
+                                                    ));
+                                                }
+                                                Ok(ev) if loading | starting => {
+                                                    debug!(?ev, "Skipping Event");
+                                                }
+                                                Ok(ev) => {
+                                                    info!(?ev, "Handling Event");
+
+                                                    return Some((
+                                                        ev,
+                                                        LinkFileScannerState::Running(
+                                                            RunningState {
+                                                                content,
+                                                                file,
+                                                                index,
+                                                                loading,
+                                                                starting,
+                                                            },
+                                                        ),
+                                                    ));
+                                                }
+                                                Err(e) => {
+                                                    error!(?e, ?line, "Could not Parse Event")
+                                                }
+                                            }
+                                        }
+                                    } else {
+                                        info!("{}", line);
+                                    }
+                                }
+                            }
+                            starting = false;
+
+                            tokio::time::sleep(Duration::from_millis(100)).await;
+
+                            content.clear();
+
+                            file.read_to_string(&mut content).await.ok()?;
+
+                            content = content
+                                .replace("':TRUE", "':true")
+                                .replace("':False", "':false")
+                                .replace("'", "\"");
+
+                            last_index = 0;
+                        }
+                    }
+                }
+            },
+        ))
+    }
+}
+
 pub async fn run(
     link_file_path: &str,
-    sender: tokio::sync::mpsc::Sender<Event>,
+    mut sender: futures::channel::mpsc::Sender<Event>,
 ) -> anyhow::Result<()> {
     let mut file = tokio::fs::File::open(link_file_path).await?;
 
---
+++ src/player_state.rs
@@ -0,0 +1,130 @@
+use iced::Subscription;
+use tracing::info;
+
+use crate::link_file::{EquipmentChanged as EqipmentState, Event, LinkFileScanner};
+
+#[derive(Debug, Clone)]
+enum GameState {
+    Running,
+    Paused,
+    NotRunning,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self::NotRunning
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    Arousal(u8),
+}
+
+#[derive(Debug, Default)]
+pub struct State {
+    arousal: u8,
+    equipment_state: EqipmentState,
+    detected_mods: Vec<String>,
+    game_status: GameState,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            arousal: Default::default(),
+            equipment_state: Default::default(),
+            detected_mods: Default::default(),
+            game_status: Default::default(),
+        }
+    }
+
+    pub fn update(&mut self, message: Message) -> iced::Command<crate::Message> {
+        iced::Command::none()
+    }
+
+    pub fn handle(&mut self, ev: crate::link_file::Event) -> iced::Command<crate::Message> {
+        match ev {
+            Event::DD(crate::link_file::DDEvent::EquipmentChanged(state)) => {
+                self.equipment_state = state;
+            }
+            Event::DD(_) => {}
+            Event::Game(crate::link_file::GameEvent::MenuClosed) => {
+                self.game_status = GameState::Running
+            }
+            Event::Game(crate::link_file::GameEvent::MenuOpened) => {
+                self.game_status = GameState::Paused
+            }
+            Event::Game(crate::link_file::GameEvent::LoadingSave(l)) => {
+                let mut mods = Vec::new();
+
+                if l.bf_running {
+                    mods.push("Being Female".to_string())
+                }
+
+                if l.dd_running {
+                    mods.push("Devious Devices".to_string())
+                }
+
+                if l.mme_running {
+                    mods.push("Milk Mod Economy".to_string())
+                }
+
+                if l.sgo_running {
+                    mods.push("Soulgem Oven".to_string())
+                }
+
+                if l.sla_running {
+                    mods.push("SexLab Aroused".to_string())
+                }
+
+                self.detected_mods = mods;
+            }
+            Event::Game(_) => {}
+            Event::Sla(crate::link_file::SLAEvent { arousal }) => {
+                self.arousal = arousal;
+            }
+            Event::Sexlab(_) => todo!(),
+            Event::MilkMod(_) => todo!(),
+        }
+        iced::Command::none()
+    }
+
+    pub fn view(&mut self) -> iced::Element<'_, crate::Message> {
+        let mut column = iced::Column::new()
+            .push(iced::Text::new(format!("Arousal: {}", self.arousal)))
+            .push(iced::Text::new(format!("Devious Devices:")))
+            .push(iced::Text::new(format!(
+                "Vaginal Plug: {:?}",
+                self.equipment_state.vaginal
+            )))
+            .push(iced::Text::new(format!(
+                "Anal Plug: {:?}",
+                self.equipment_state.anal
+            )))
+            .push(iced::Text::new(format!(
+                "Vaginal Piercing: {:?}",
+                self.equipment_state.vaginal_piercing
+            )))
+            .push(iced::Text::new(format!(
+                "Nipple Piercing: {:?}",
+                self.equipment_state.nipple_piercing
+            )))
+            .push(iced::Text::new(format!("Mods Detected:")));
+
+        for detected_mod in self.detected_mods.iter() {
+            column = column.push(iced::Text::new(detected_mod.clone()));
+        }
+
+        column = column.push(iced::Text::new(format!("Game State: {:?}", self.game_status)));
+
+        iced::Container::new(column).into()
+    }
+
+    pub fn subscription(&self) -> iced::Subscription<crate::Message> {
+        Subscription::from_recipe(LinkFileScanner::new(
+            "E:\\ModOrganizer2\\SSE\\mods\\Butthesda\\FunScripts\\link.txt".to_string(),
+        ))
+        .map(|e| crate::Message::FileEvent(e))
+    }
+}
