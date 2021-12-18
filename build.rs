61e2b00cd3816adaac31930029a068f0c26a28bc
Lucky-Skebe <fmann12345@gmail.com>
2021-12-18 12:12:43 +0100

    feat: remove console in release builds

--- build.rs
+++ build.rs
@@ -2,10 +2,10 @@ extern crate embed_resource;
 
 fn main() {
 
-    #[cfg(debug_assertions)]
+    // #[cfg(debug_assertions)]
     embed_resource::compile("debug.rc");
 
     
-    #[cfg(not(debug_assertions))]
-    embed_resource::compile("release.rc");
+    // #[cfg(not(debug_assertions))]
+    // embed_resource::compile("release.rc");
 }
